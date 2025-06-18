858a356f9992075f9471b3cb22ca449c34bc6b57
Luca324 <tanya.mosol@yandex.ru>
2025-06-18 13:02:38 +0300

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
-import { useState } from 'react'
-import './App.css'
-import Generator from './pages/Generator/Generator'
-import Uploader from './pages/Uploader/Uploader'
-import History from './pages/History/History'
-import Header from './components/header/Header'
-function App() {
+import { useState } from "react";
+import "./App.css";
+import Generator from "./pages/Generator/Generator";
+import Uploader from "./pages/Uploader/Uploader";
+import History from "./pages/History/History";
+import Header from "./components/header/Header";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Uploader></Uploader>
-      {/* <Generator></Generator> */}
-      {/* <History></History> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Uploader />} />
+          <Route path="/uploader" element={<Uploader />} />
+          <Route path="/generator" element={<Generator />} />
+          <Route path="/history" element={<History />} />
+        </Routes>
+      </BrowserRouter>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
